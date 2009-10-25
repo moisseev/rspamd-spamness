04354ad62e7cdcde64e7d184c3668b5faa573553
voccs <ryan@voccs.com>
2009-10-25 00:45:51 +0000

    new prefs
    git-svn-id: file:///opt/svn/spamness/src@11 7fb02860-a0bc-471d-b2fe-d43feb07dad5

--- defaults/preferences/spamness.js
+++ defaults/preferences/spamness.js
@@ -1,2 +1,4 @@
 pref("mailnews.customDBHeaders", "x-spam-status");
 pref("mailnews.customHeaders", "x-spam-status");
+pref("extensions.org.ryanlee.spamness.SAHeader", "x-spam-status");
+pref("extensions.org.ryanlee.spamness.columnDisplay", 0);
