3b35305185457899bd4fee0f7457615c22843ef9
Alexander Moisseev <moiseev@mezonplus.ru>
2015-12-03 18:05:18 +0300

    Change default options

--- defaults/preferences/spamness.js
+++ defaults/preferences/spamness.js
@@ -1,7 +1,7 @@
-pref("extensions.spamness.header", "x-spam-status");
+pref("extensions.spamness.header", "x-spamd-result");
 pref("extensions.spamness.display.column", 0);
 pref("extensions.spamness.display.messageScore", true);
-pref("extensions.spamness.display.messageRules", false);
+pref("extensions.spamness.display.messageRules", true);
 pref("extensions.spamness.version", "0.6");
 pref("extensions.spamness.isDefaultColumn", true);
 pref("extensions.spamness.installationGreeting", true);
