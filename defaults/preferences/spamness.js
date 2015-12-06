8cc25df2908a365b5a4c982c69f51ba7cf17847e
Alexander Moisseev <moiseev@mezonplus.ru>
2015-12-06 19:06:39 +0300

    Remove `version` from default preferences

--- defaults/preferences/spamness.js
+++ defaults/preferences/spamness.js
@@ -2,6 +2,5 @@ pref("extensions.rspamd-spamness.header", "x-spamd-result");
 pref("extensions.rspamd-spamness.display.column", 0);
 pref("extensions.rspamd-spamness.display.messageScore", true);
 pref("extensions.rspamd-spamness.display.messageRules", true);
-pref("extensions.rspamd-spamness.version", "0.6");
 pref("extensions.rspamd-spamness.isDefaultColumn", true);
 pref("extensions.rspamd-spamness.installationGreeting", true);
