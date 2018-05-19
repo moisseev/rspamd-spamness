c2d4494d7f9eeddea4d636f646ebba669528d7bb
Alexander Moisseev <moiseev@mezonplus.ru>
2018-05-19 21:18:26 +0300

    Group symbols by default

--- defaults/preferences/spamness.js
+++ defaults/preferences/spamness.js
@@ -7,7 +7,7 @@ pref("extensions.rspamd-spamness.display.columnImageOnlyForPositive", false);
 pref("extensions.rspamd-spamness.display.messageScore", true);
 pref("extensions.rspamd-spamness.display.messageRules", true);
 pref("extensions.rspamd-spamness.display.messageGreylist", true);
-pref("extensions.rspamd-spamness.headers.group_symbols", false);
+pref("extensions.rspamd-spamness.headers.group_symbols", true);
 pref("extensions.rspamd-spamness.headers.show_n_lines_before_more", 2);
 pref("extensions.rspamd-spamness.headers.colorizeSymbols", true);
 pref("extensions.rspamd-spamness.headers.symbols_order", "score");
