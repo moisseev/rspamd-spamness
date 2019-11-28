0dd3be532bf78df1a1b5391abc67bb965e9a5a20
Remco de Man <gitlab@remcodeman.nl>
2019-11-28 21:10:41 +0100

    Try built in method to ensure rules dialog is centered when opened.
    Use Dialog.centerWindowOnScreen() to center the rules dialog after opening
    instead of using the old manual method, which does not work anymore because
    the width en height attributes are always undefined (Thunderbird bug?).

--- chrome/content/scripts/rules-dialog.js
+++ chrome/content/scripts/rules-dialog.js
@@ -1,15 +1,8 @@
 function resizeTextbox() {
     let box = document.getElementById("rulesDlgTextbox");
     box.value = window.arguments[0];
+    box.style.cssText = 'background-color:transparent; overflow:hidden; height:' + box.scrollHeight + 'px';
 
-    setTimeout(() => {
-        box.style.cssText = 'background-color:transparent; overflow:hidden; height:' + box.scrollHeight + 'px';
-console.log(box.scrollHeight);
-console.log(box.style.cssText);
-
-        // Center the dialog window
-        const w = screen.availWidth/2 - this.width/2;
-        const h = screen.availHeight/2 - this.height/2;
-        window.moveTo(w,h);
-    }, 0);
+    // Center the dialog window
+    this.centerWindowOnScreen();
 }
